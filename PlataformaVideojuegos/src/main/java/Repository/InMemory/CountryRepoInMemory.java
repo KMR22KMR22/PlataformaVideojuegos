b7954dbfa0e8dab4bebb086bceeb6c37c27a8283
Kevin <k.mendez4229@gmail.com>
2026-02-13 01:20:00 -0400

    Organice las validaciones del usuario y del juego

---
+++ PlataformaVideojuegos/src/main/java/Repository/InMemory/CountryRepoInMemory.java
@@ -0,0 +1,16 @@
+package Repository.InMemory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryRepoInMemory{
+
+    private List<String> countries = new ArrayList<>();
+
+    public List<String>  getCountries() {
+        return countries;
+    }
+    public void addCountries(String country) {
+        countries.add(country);
+    }
+}
